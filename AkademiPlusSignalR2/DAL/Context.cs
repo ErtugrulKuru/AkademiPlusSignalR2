e6dd55c748a29df73d648cf6765940bb1bfb0254
Hamza Ertugrul Kuru <hertugrulkuru@gmail.com>
2023-07-04 21:20:40 +0300

    Add project files.

---
+++ AkademiPlusSignalR2/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace AkademiPlusSignalR2.DAL
+{
+    public class Context:DbContext        
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-JH4NS85;initial catalog=DbSignalR2;integrated security=true");
+        }
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
